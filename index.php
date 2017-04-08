6e624579ff98f7beb4e795e4cef86d25febdfd61
Antonin RIDOLFI <aridolfi@student.42.fr>
2017-04-08 11:48:13 +0200

    add index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+
+
+?>
+<!DOCTYPE html>
+<html>
+	<head lang="fr">
+		<meta charset="utf-8">
+		<title>ft_amazon</title>
+		<link rel="stylesheet" type="text/css" href="#" />
+	</head>
+	<body>
+		<a href="./create.php">Inscription</a>
+	</body>
+</html>
