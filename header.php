522477efeb6226eca1806ed699a2b0ea75c112de
Alexia TOULMET <atoulmet@e2r3p9.42.fr>
2017-04-08 23:40:23 +0200

    css

--- header.php
+++ header.php
@@ -1,17 +1,15 @@
 <header>
-	<?php
-
-		if ($_SESSION['loggued_on_user'])
-	   	{
-			echo "Bonjour ".$_SESSION['loggued_on_user'];
-			echo '<a href="logout.php">Logout</a>';
-			if ($_SESSION['admin'] === 'true')
-				echo "Mighty administrator";
-		}
-		else
-	  	{
-			echo '<a href="login.php">Login</a>';
-		}
-		
-	?>
+<?php
+	if ($_SESSION['loggued_on_user'])
+   	{
+		if ($_SESSION['admin'] === 'true')
+			echo "<div class = 'status'>Mighty administrator\n</div>";
+		echo "Bonjour ".$_SESSION['loggued_on_user'];
+		echo "<div class = 'account'><a href='logout.php'>Logout</a></div>";
+	}
+	else
+  	{
+		echo '<a href="login.php">Login</a>';
+	}
+?>
 </header>
--- index.php
+++ index.php
@@ -12,14 +12,15 @@ if (session_start() === false)
 	<head lang="fr">
 		<meta charset="utf-8">
 		<title>ft_amazon</title>
-		<link rel="stylesheet" type="text/css" href="#" />
+		<link rel="stylesheet" type="text/css" href="style.css" />
 	</head>
-	
+	<h1>Bienvenue sur la boutique 42</h1>	
 	<body>
 		<?php include_once("header.php") ?>
-
+		<div class="account">
 		<a href="sign_up.php">Inscription</a>
-
+		</br>
 		<a href="modif_passwd.php">Modifier le mot de passe</a>
+		</div>
 	</body>
 </html>
