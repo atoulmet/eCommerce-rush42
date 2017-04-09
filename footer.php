1698e8be2e204371d2ee9c21e798a20281ab4be4
Antonin RIDOLFI <aridolfi@student.42.fr>
2017-04-09 10:17:04 +0200

    Add footer

---
+++ footer.php
@@ -0,0 +1,3 @@
+<footer>
+	<small>© copyright 2017 - by <em>atoulmet</em> & <em>aridolfi</em> - All rights reserved.</small>
+</footer>
--- index.php
+++ index.php
@@ -12,15 +12,19 @@ if (session_start() === false)
 	<head lang="fr">
 		<meta charset="utf-8">
 		<title>ft_amazon</title>
-		<link rel="stylesheet" type="text/css" href="style.css" />
+		<link rel="stylesheet" type="text/css" href="css/style.css" />
 	</head>
 	<h1>Bienvenue sur la boutique 42</h1>
 	<body>
 		<?php include_once("header.php"); ?>
+
 		<div class="account">
-		<a href="sign_up.php">Inscription</a>
-		</br>
-		<a href="modif_passwd.php">Modifier le mot de passe</a>
+			<a href="sign_up.php">Inscription</a>
+			</br>
+
+			<a href="modif_passwd.php">Modifier le mot de passe</a>
 		</div>
+
+		<?php include_once("footer.php"); ?>
 	</body>
 </html>
