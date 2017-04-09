04c63b3729a822f94d8f6fc3ee682100859aad3c
Alexia TOULMET <atoulmet@e2r3p9.42.fr>
2017-04-09 21:04:41 +0200

    panier

--- panier.php
+++ panier.php
@@ -2,15 +2,14 @@
 
 if (session_start() === false)
 {
-	    echo "Erreur inattendue\n";
-		    exit ;
+	echo "Erreur inattendue\n";
+	exit ;
 }
 if ($_POST['add_panier'])
 {
-	if (!isset($_SESSION['panier'])
+	if (!isset($_SESSION['panier']))
 		$_SESSION['panier'] = array_merge(array(), $_POST['add_panier']);
 	else
 		$_SESSION['panier'] = array_merge($_SESSION['panier'], $_POST['add_panier']);
 }
-
 ?>
---
+++ show_panier.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head lang="fr">
+        <meta charset="utf-8">
+        <title>ft_amazon</title>
+        <link rel="stylesheet" type="text/css" href="css/style.css" />
+    </head>
+
+        <body>
+                <?php
+
+                    include_once("header.php");
+
+					                ?>
+
+            <div class="cell">
+
+                </div>
+                    </div>
+
+        <?php include_once("footer.php"); ?>
+    </body>
+</html>
